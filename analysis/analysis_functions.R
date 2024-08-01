dbaf1c0a54a81b6fa758f82270e75e7d6955a972
Lev <woodenrobo@gmail.com>
2024-08-01 14:49:04 +0200

    added examples of usage for summary_table and calculate_cluster_proportions to analysis_core

--- analysis/analysis_functions.R
+++ analysis/analysis_functions.R
@@ -681,12 +681,5 @@ calculate_cluster_proportions <- function(cluster_var = "meta_cluster_id", selec
 }
 
 
-summary_table(exprs_set, c(group), selected_features = NULL, "n_size")
-summary_table(exprs_set, c(group, "id"), selected_features = clustering_feature_markers, "mean")
-summary_table(exprs_set, c(group, "id"), selected_features = clustering_feature_markers, "median")
 
-cluster_var <- "meta_cluster_id"
-calculate_cluster_proportions(cluster_var = cluster_var, selected_clusters = unique(exprs_set[[cluster_var]]))
-cluster_var <- "meta_cluster_annotation"
-calculate_cluster_proportions(cluster_var = cluster_var, selected_clusters = unique(exprs_set[[cluster_var]]))
 
